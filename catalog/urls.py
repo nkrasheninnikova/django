cbbe8ddc2860af5b891097a8869db141af6e29e0
Natasha Krasheninnikova <nk110905@mail.ru>
2024-11-20 22:48:56 +0700

    eleven commit

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
-from django.conf.urls import url
+from django.urls import re_path
 
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
-    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    re_path(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
 
 
