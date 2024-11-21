2ce64478737d136b79c8bd2de17b85cc26483c3f
Наталья Крашенинникова <gr431_krnvi@lab-402-4.tomtit-tomsk.ru>
2024-11-21 13:45:55 +0700

    sixteen commit

--- catalog/models.py
+++ catalog/models.py
@@ -77,6 +77,8 @@ class Book(models.Model):
         return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
     display_genre.short_description = 'Genre'
 
+
+
 class BookInstance(models.Model):
     """
     Model representing a specific copy of a book (i.e. that can be borrowed from the library).
