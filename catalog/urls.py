c32e955ec44ee2e43cfca7c1d9a1aa7fbbd0c659
Наталья Крашенинникова <gr431_krnvi@lab-402-4.tomtit-tomsk.ru>
2024-11-21 09:33:31 +0700

    twelve commit

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
     re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
     re_path(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
 
 
